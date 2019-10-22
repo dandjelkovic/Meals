919d6fff8d7080797ae81079fc6fef5f4ae96abb
Dali Andjelkovic <dan@adorsys.de>
2019-10-22 12:31:20 +0200

    Initial Commit

---
+++ Meals/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Meals
+//
+//  Created by Dalibor Andjelkovic on 22.10.19.
+//  Copyright © 2019 Dalibor Andjelkovic. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

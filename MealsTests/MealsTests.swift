c87fae017c076487d0184412fbcbf6be0d852879
Dali Andjelkovic <dan@adorsys.de>
2019-10-24 11:10:34 +0200

    Add non working unit test

--- MealsTests/MealsTests.swift
+++ MealsTests/MealsTests.swift
@@ -7,28 +7,76 @@
 //
 
 import XCTest
+import CoreData
 @testable import Meals
 
 class MealsTests: XCTestCase {
+    var context: NSManagedObjectContext!
 
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
+        context = setupCoreDataStack()
+        let today = Current.date()
+        for _ in 1...3 {
+            let meal = Meal(entity: Meal.entity(), insertInto: context)
+            meal.weight = [Weight.light, Weight.medium, Weight.heavy].randomElement()!
+            meal.timestamp = today
+            try! context.save()
+        }
+        for _ in 1...3 {
+            let meal = Meal(entity: Meal.entity(), insertInto: context)
+            meal.weight = [Weight.light, Weight.medium, Weight.heavy].randomElement()!
+            meal.timestamp = Calendar.current.date(byAdding: .day, value: -1, to: today)!
+            try! context.save()
+        }
+        for _ in 1...3 {
+            let meal = Meal(entity: Meal.entity(), insertInto: context)
+            meal.weight = [Weight.light, Weight.medium, Weight.heavy].randomElement()!
+            meal.timestamp = Calendar.current.date(byAdding: .day, value: -2, to: today)!
+            try! context.save()
+        }
+
+//        lookupMeals()
+    }
+
+    private func setupCoreDataStack() -> NSManagedObjectContext {
+        //        let testContainer = NSPersistentContainer(name: "MockMeals")
+        //        testContainer.loadPersistentStores(completionHandler: { (storeDescription, error) in
+        //            if let error = error as NSError? {
+        //                fatalError("Unresolved error \(error), \(error.userInfo)")
+        //            }
+        //        })
+        //        let modelURL = Bundle.main.url(forResource: "Meals", withExtension: "xcdatamodeld")!
+        let model: NSManagedObjectModel = NSManagedObjectModel.mergedModel(from: [Bundle.main])!
+        let coordinator = NSPersistentStoreCoordinator(managedObjectModel: model)
+        try! coordinator.addPersistentStore(ofType: NSInMemoryStoreType, configurationName: nil, at: nil, options: nil)
+
+        let managedObjectContext = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+        managedObjectContext.persistentStoreCoordinator = coordinator
+        return managedObjectContext
+    }
+
+    private func lookupMeals() {
+        do {
+            let fetchRequest : NSFetchRequest<Meal> = Meal.fetchRequest()
+            let fetchedResults = try context.fetch(fetchRequest) 
+            print(fetchedResults)
+
+        }
+        catch {
+            print ("fetch task failed", error)
+        }
     }
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testdaysByRowOfThree() {
+        let statisticsView = Statistics()
+        XCTAssertEqual(statisticsView.daysByRowOfThree.count, 3)
+        
     }
 
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
 
 }
